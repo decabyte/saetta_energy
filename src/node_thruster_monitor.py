5635f6edc731e3e77068c31faf12f2e94a23ebc4
Valerio De Carolis <valerio.decarolis@gmail.com>
2014-11-24 12:18:13 +0000

    update thruster diagnostics with new smoothing filter

--- src/node_thruster_monitor.py
+++ src/node_thruster_monitor.py
@@ -22,27 +22,31 @@ TOPIC_MODEL = 'thrusters/model'
 TOPIC_REAL = 'thrusters/status'
 TOPIC_DIAG = 'thrusters/diagnostics'
 
-TOPIC_ENERGY_REAL = 'thrusters/energy/real'
-TOPIC_ENERGY_MODEL = 'thrusters/energy/model'
+# TOPIC_ENERGY_REAL = 'thrusters/energy/real'
+# TOPIC_ENERGY_MODEL = 'thrusters/energy/model'
 
 # configs
-SAMPLE_WINDOW = 20          # samples
+RATE_MONITOR = 10           # Hz
+WIN_CURR = 12               # samples (1 sec + two samples for fixing ROS delay)
+WIN_DIAG = 20               # samples
 SAMPLE_TIME = 0.1           # secs
 NOMINAL_VOLTAGE = 28.0      # assumed constant
 
-MONITOR_RATE = 10           # Hz
-ENERGY_RATE = 2             # Hz
-
 
 class ThrustersMonitor(object):
-    def __init__(self, name, topic_input_req, topic_input_real, sample_window, **kwargs):
+    def __init__(self, name, topic_input_req, topic_input_real, win_curr, **kwargs):
         self.name = name
-        self.rate = rospy.Rate(MONITOR_RATE)
+        self.rate = rospy.Rate(RATE_MONITOR)
 
-        self.sample_window = sample_window
-        self.real_current = np.zeros((6, sample_window))
-        self.model_current = np.zeros((6, sample_window))
-        self.diag_metric = np.zeros(6)
+        self.win_curr = win_curr
+        self.real_current = np.zeros((6, win_curr))
+        self.model_current = np.zeros((6, win_curr))
+
+        self.diagnostic_vector = np.zeros((6, WIN_DIAG))
+        self.diagnostic_metric = np.zeros(6)
+
+        self.exp_weights = np.exp(np.linspace(-1.0, 0.0, WIN_DIAG))
+        self.exp_weights = self.exp_weights / np.sum(self.exp_weights)
 
         self.throttle_request = np.zeros((6, tc.LPF_WINDOW))
         self.last_throttle = np.zeros(6)
@@ -68,21 +72,19 @@ class ThrustersMonitor(object):
         # self.n = 0
         # self.data = np.zeros((3, 100))
 
-
         # subscribers
         self.sub_req = rospy.Subscriber(topic_input_req, ThrusterCommand, self.handle_req, tcp_nodelay=True, queue_size=3)
         self.sub_status = rospy.Subscriber(topic_input_real, ThrusterFeedback, self.handle_status, tcp_nodelay=True, queue_size=3)
 
         # publishers
-        self.pub_model = rospy.Publisher(TOPIC_MODEL, ThrusterFeedback, tcp_nodelay=True, queue_size=1)
-        self.pub_diag = rospy.Publisher(TOPIC_DIAG, FloatArrayStamped, tcp_nodelay=True, queue_size=1)
-
+        self.pub_model = rospy.Publisher(TOPIC_MODEL, ThrusterFeedback, tcp_nodelay=True, queue_size=3)
+        self.pub_diag = rospy.Publisher(TOPIC_DIAG, FloatArrayStamped, tcp_nodelay=True, queue_size=3)
 
 
     def send_diagnostics(self):
         msg = FloatArrayStamped()
         msg.header.stamp = rospy.Time.now()
-        msg.values = self.diag_metric
+        msg.values = self.diagnostic_metric
         self.pub_diag.publish(msg)
 
     def send_model(self):
@@ -92,95 +94,110 @@ class ThrustersMonitor(object):
         msg.current = self.model_current[:, -1]
         self.pub_model.publish(msg)
 
+
+    # exponential smoothing
+    def weighted_moving_average(data, weights):
+        s = np.zeros(data.shape[1])
+
+        if data.ndim == 2:
+            for n in xrange(data.shape[1]):
+                s[:, n] = np.convolve(data[:,n], w, 'same')
+        else:
+            s = np.convolve(data, w, 'same')
+
+        return s
+
+
     def run(self):
         while not rospy.is_shutdown():
+            # predict throttle
+            self.predicted_throttle = th.predict_throttle(
+                self.throttle_request, tc.LPF[0], tc.LPF[1], self.model_delay, tc.MAX_THROTTLE
+            )
+
+            # shift input buffer and zero the new entry (in case no further messages are sent from the controller)
+            self.throttle_request = np.roll(self.throttle_request, -1, axis=1)
+            #self.throttle_request[:, -1] = np.zeros(6)
+
+            # rate limiter
+            self.predicted_throttle = th.rate_limiter(
+                self.predicted_throttle, self.last_throttle,
+                rising_limit=tc.THROTTLE_RISING_LIMIT, falling_limit=tc.THROTTLE_FALLING_LIMIT
+            )
+
+            # store previous throttle value
+            self.last_throttle = self.predicted_throttle
+
+            # store predicted current
+            self.model_current = np.roll(self.model_current, -1, axis=1)
+            self.model_current[:, -1] = tm.estimate_current(self.predicted_throttle, tc.THROTTLE_TO_CURRENT)
+
+            # calculate the energies over the sample window (fix for ROS delay)
+            self.model_energy = np.trapz(self.model_current[:, 2:], dx=SAMPLE_TIME, axis=1) * NOMINAL_VOLTAGE
+            self.real_energy = np.trapz(self.real_current[:, :-2], dx=SAMPLE_TIME, axis=1) * NOMINAL_VOLTAGE
+
+            # calculate diagnostic metric (without including delays)
+            self.diagnostic_vector[:,-1] = (self.model_energy - self.real_energy)
+
+            # exponential moving average filter
+            for n in xrange(self.diagnostic_vector.shape[0]):
+                self.diagnostic_metric[n] = np.convolve(self.diagnostic_vector[n,:], self.exp_weights, mode='same')[-1]
+
+            # advance filter
+            self.diagnostic_vector = np.roll(self.diagnostic_vector, -1, axis=1)
+
+            # shift status buffer and zero the new entry (in case no further messages are sent from the controller)
+            self.real_current = np.roll(self.real_current, -1, axis=1)
+            #self.real_current[:, -1] = np.zeros(6)
+
             self.send_diagnostics()
             self.send_model()
 
             self.rate.sleep()
 
+            ## data fusion (using kalman filter)
+            # measurements = np.array([
+            #     # self.real_current[0, -1],
+            #     # self.model_current[0, -1],
+            #
+            #     self.model_current[0, -1],
+            #     self.real_current[0, -1]
+            # ])
+            # mean, cov = self.kf.update(measurements)
+            #
+            # print('mean: %s' % mean)
+            # print('cov: %s' % cov)
+            #
+            # # store data
+            # self.data[0, self.n] = self.model_current[0, -1]
+            # self.data[1, self.n] = self.real_current[0, -1]
+            # self.data[2, self.n] = mean
+            #
+            # self.n += 1
+            #
+            # if self.n >= self.data.shape[1]:
+            #     print('kf plot')
+            #
+            #     import matplotlib.pyplot as plt
+            #
+            #     t = np.linspace(0, 10, self.data.shape[1])
+            #
+            #     plt.figure()
+            #     plt.plot(t, self.data[0,:], 'b--', label='sensor #1')
+            #     plt.plot(t, self.data[1,:], 'g--', label='sensor #2')
+            #     plt.plot(t, self.data[2,:], 'r', label='fusion')
+            #     plt.legend()
+            #     plt.show()
+            #
+            #     rospy.signal_shutdown('kf end')
 
     def handle_req(self, data):
         # parse input request
         self.throttle_request[:, -1] = np.array(data.throttle)
 
-        # predict throttle
-        self.predicted_throttle = th.predict_throttle(
-            self.throttle_request, tc.LPF[0], tc.LPF[1], self.model_delay, tc.MAX_THROTTLE
-        )
-
-        # shift input buffer and zero the new entry (in case no further messages are sent from the controller)
-        self.throttle_request = np.roll(self.throttle_request, -1, axis=1)
-        self.throttle_request[:, -1] = np.zeros(6)
-
-        # rate limiter
-        self.predicted_throttle = th.rate_limiter(
-            self.predicted_throttle, self.last_throttle,
-            rising_limit=tc.THROTTLE_RISING_LIMIT, falling_limit=tc.THROTTLE_FALLING_LIMIT
-        )
-
-        # store previous throttle value
-        self.last_throttle = self.predicted_throttle
-
-        # store predicted current
-        self.model_current = np.roll(self.model_current, -1, axis=1)
-        self.model_current[:, -1] = tm.estimate_current(self.predicted_throttle, tc.THROTTLE_TO_CURRENT)
-
-        # calculate the energy for model (using the last two samples)
-        #self.model_energy[0:6] += np.trapz(self.model_current[:, -2:], dx=SAMPLE_TIME) * NOMINAL_VOLTAGE
-
-        # TODO: fix this with a proper window of N samples (10 samples is fine but we need to change the thresholds in the pilot)
-        # compensate for time delays
-        self.diag_metric = np.trapz(self.model_current[:, 0:-2] - self.real_current[:, 1:-1], dx=SAMPLE_TIME)
-
-        # # data fusion (using kalman filter)
-        # measurements = np.array([
-        #     # self.real_current[0, -1],
-        #     # self.model_current[0, -1],
-        #
-        #     self.model_current[0, -1],
-        #     self.real_current[0, -1]
-        # ])
-        # mean, cov = self.kf.update(measurements)
-        #
-        # print('mean: %s' % mean)
-        # print('cov: %s' % cov)
-        #
-        # # store data
-        # self.data[0, self.n] = self.model_current[0, -1]
-        # self.data[1, self.n] = self.real_current[0, -1]
-        # self.data[2, self.n] = mean
-        #
-        # self.n += 1
-        #
-        # if self.n >= self.data.shape[1]:
-        #     print('kf plot')
-        #
-        #     import matplotlib.pyplot as plt
-        #
-        #     t = np.linspace(0, 10, self.data.shape[1])
-        #
-        #     plt.figure()
-        #     plt.plot(t, self.data[0,:], 'b--', label='sensor #1')
-        #     plt.plot(t, self.data[1,:], 'g--', label='sensor #2')
-        #     plt.plot(t, self.data[2,:], 'r', label='fusion')
-        #     plt.legend()
-        #     plt.show()
-        #
-        #     rospy.signal_shutdown('kf end')
-
-
-        # shift status buffer and zero the new entry (in case no further messages are sent from the controller)
-        self.real_current = np.roll(self.real_current, -1, axis=1)
-        self.real_current[:, -1] = np.zeros(6)
-
-
     def handle_status(self, data):
         # parse sensor data
         self.real_current[:, -1] = np.array(data.current)
-
-        # calculate the energy for real (using the last two samples)
-        #self.real_energy[0:6] += np.trapz(self.real_current[:, -2:], dx=SAMPLE_TIME) * NOMINAL_VOLTAGE
 
 
 def main():
@@ -190,7 +207,7 @@ def main():
 
     # config
     config = rospy.get_param('thruster_model', dict())
-    sample_window = int(rospy.get_param('~sample_window', SAMPLE_WINDOW))
+    samples_current = int(rospy.get_param('~samples_current', WIN_CURR))
 
     topic_input_req = rospy.get_param('~topic_input_req', TOPIC_REQ)
     topic_input_real = rospy.get_param('~topic_input_real', TOPIC_REAL)
@@ -201,10 +218,10 @@ def main():
     rospy.loginfo('%s: topic input req: %s', rospy.get_name(), topic_input_req)
     rospy.loginfo('%s: topic input real: %s', rospy.get_name(), topic_input_real)
     rospy.loginfo('%s: topic diagnostics: %s', rospy.get_name(), topic_diagnostics)
-    rospy.loginfo('%s: sample window: %s', rospy.get_name(), sample_window)
+    rospy.loginfo('%s: current samples: %s', rospy.get_name(), samples_current)
 
     # init monitor
-    tm = ThrustersMonitor(name, topic_input_req, topic_input_real, sample_window, **config)
+    tm = ThrustersMonitor(name, topic_input_req, topic_input_real, samples_current, **config)
 
     try:
         tm.run()
