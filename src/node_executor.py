9b4bf2a018815850a478ce76ec806ee9e55d0f3c
Valerio De Carolis <valerio.decarolis@gmail.com>
2015-07-02 12:30:38 +0100

    update executor with direction and distance logging

--- src/node_executor.py
+++ src/node_executor.py
@@ -23,12 +23,14 @@ from vehicle_core.path import trajectory_tools as tt
 from saetta_energy import tsp
 from saetta_energy.utils import id_generator, urlify
 
+from auv_msgs.msg import NavSts
 from vehicle_interface.msg import PathRequest, PathStatus, PilotStatus, Vector6
 from vehicle_interface.srv import BooleanService, PathService, PathServiceRequest, PathServiceResponse
 from saetta_energy.msg import EnergyReport, EnergyStatus
 from diagnostic_msgs.msg import KeyValue
 
 
+TOPIC_NAV = 'nav/sts'
 TOPIC_PATH_REQ = 'path/request'
 TOPIC_PATH_STS = 'path/status'
 TOPIC_PATH_SRV = 'path/control'
@@ -60,9 +62,11 @@ class MissionExecutor(object):
 
         # mission data
         self.config = None
+        self.plan_id = 0
         self.ips = []
         self.actions = []
 
+        # estimation config
         self.e_perf = 300.0          # navigation cost (J/m)
 
         # mission state machine
@@ -79,6 +83,9 @@ class MissionExecutor(object):
         self.state_action = ACTION_IDLE
         self.last_action = None
 
+        # vehicle state
+        self.pos = np.zeros(6, dtype=np.float64)
+
         # path monitoring
         self.current_path_id = -1
         self.last_path_id = 0
@@ -93,17 +100,21 @@ class MissionExecutor(object):
         }
 
         # output
-        self.label = 'current'
         self.output_dir = kwargs.get('output_dir', os.path.expanduser('~'))
-        self.output_log = None
+        self.output_label = kwargs.get('output_label', 'current')
+        self.output_log = os.path.join(self.output_dir, '{}_{}.csv'.format(self.output_label, id_generator(6)))
 
+        # action stats
         self.time_start = 0.0
         self.time_end = 0.0
         self.time_elapsed = 0.0
         self.energy_last = 0.0
         self.energy_start = 0.0
+        self.last_los_orient = 0.0
+        self.last_los_dist = 0.0
 
         # ros interface
+        self.sub_nav = rospy.Subscriber(TOPIC_NAV, NavSts, self.handle_nav, queue_size=1)
         self.pub_path = rospy.Publisher(TOPIC_PATH_REQ, PathRequest, queue_size=1)
         self.sub_path = rospy.Subscriber(TOPIC_PATH_STS, PathStatus, self.handle_path_status, queue_size=10)
         self.srv_path = rospy.ServiceProxy(TOPIC_PATH_SRV, PathService)
@@ -122,7 +133,7 @@ class MissionExecutor(object):
             rospy.loginfo('%s: saving mission output to file (%s)', self.name, self.output_log)
 
             with open(self.output_log, 'wt') as mlog:
-                mlog.write('name,action,time_start,time_end,time_elapsed,energy_used\n')
+                mlog.write('name,action,time_start,time_end,time_elapsed,energy_used,direction,distance\n')
 
     def _write_log(self, label, action):
         # record action stats
@@ -130,14 +141,30 @@ class MissionExecutor(object):
         self.time_elapsed = self.time_end - self.time_start
         self.energy_used = self.energy_last - self.energy_start
 
+        extra = '0.0,0.0'
+
+        if action['name'] == ACT_GOTO:
+            extra ='{},{}'.format(self.last_los_orient, self.last_los_dist)
+
         # generate mission log
-        out = '{},{},{},{},{},{}\n'.format(
-            label, action, self.time_start, self.time_end, self.time_elapsed, self.energy_used
+        out = '{},{},{},{},{},{},{}\n'.format(
+            label, action['name'], self.time_start, self.time_end, self.time_elapsed, self.energy_used, extra
         )
 
         # save mission log
         with open(self.output_log, 'at') as mlog:
             mlog.write(out)
+
+    def handle_nav(self, data):
+        # parse navigation data
+        self.pos = np.array([
+            data.position.north,
+            data.position.east,
+            data.position.depth,
+            data.orientation.roll,
+            data.orientation.pitch,
+            data.orientation.yaw
+        ])
 
     def handle_energy(self, data):
         self.energy_last = data.energy_used
@@ -180,23 +207,26 @@ class MissionExecutor(object):
         rospy.loginfo('%s: dispatching action[%d]: %s', self.name, self.action_id, action['name'])
 
         # action stats
-        self.last_action = action['name']
+        self.last_action = action
         self.time_start = time.time()
         self.energy_start = self.energy_last
 
         if action['name'] == ACT_GOTO:
+            goal = action['params']['pose']
             poses = []
-            poses.append(action['params']['pose'])
-            poses.append(action['params']['pose'])
+            poses.append(goal)
+            poses.append(goal)
 
-            # TEMP: mock the action system with path requests
+            self.last_los_orient = tt.calculate_orientation(self.pos, goal)
+            self.last_los_dist = tt.distance_between(self.pos, goal, spacing_dim=3)
+
             self.send_path_request(poses)
         elif action['name'] == ACT_HOVER:
+            goal = action['params']['pose']
             poses = []
-            poses.append(action['params']['pose'])
-            poses.append(action['params']['pose'])
+            poses.append(goal)
+            poses.append(goal)
 
-            # TEMP: mock the action system with path requests
             self.send_path_request(poses, mode='simple')
         else:
             rospy.logerr('%s: unknown action: %s', self.name, action)
@@ -208,7 +238,7 @@ class MissionExecutor(object):
             return
 
         # record action stats
-        self._write_log(self.label, self.last_action)
+        self._write_log(self.output_label, self.last_action)
 
     # TEMP: mock the action system with path requests
     def handle_path_status(self, data):
@@ -229,6 +259,7 @@ class MissionExecutor(object):
                 self.path_enabled = False
                 self.handle_feedback(ACTION_ACHIEVED)
 
+    # TEMP: mock the action system with path requests
     def send_path_request(self, poses, **kwargs):
         self.path_enabled = True
 
@@ -277,12 +308,14 @@ class MissionExecutor(object):
 
         # store config
         self.config = config
-        self.label = config.get('output_label', 'current')
-        self.output_log = os.path.join(self.output_dir, '{}_{}.csv'.format(self.label, id_generator(6)))
+
+        if config.get('output_label', None) is not None:
+            self.output_label = config['output_label']
+            self.output_log = os.path.join(self.output_dir, '{}_{}.csv'.format(self.output_label, id_generator(6)))
 
         # init mission
         self.state_mission = MISSION_IDLE
-        self._init_log(self.label)
+        self._init_log(self.output_label)
 
         # initial plan
         self.ips = {'IP_%d' % n: inspection_points[n, :] for n in xrange(inspection_points.shape[0])}
@@ -296,9 +329,9 @@ class MissionExecutor(object):
 
     def _plan(self):
         # select ips to visit
-        ip_labels = [ip for ip, s in self.ips_state.iteritems() if s == 0]
+        ips_labels = [ip for ip, s in self.ips_state.iteritems() if s == 0]
 
-        k = len(ip_labels)
+        k = len(ips_labels)
         self.ips_costs = np.zeros((k, k))
 
         # update costs
@@ -307,28 +340,68 @@ class MissionExecutor(object):
                 if i == j:
                     continue
 
-                dist = tt.distance_between(self.ips[ip_labels[i]], self.ips[ip_labels[j]], spacing_dim=3)
+                dist = tt.distance_between(self.ips[ips_labels[i]], self.ips[ips_labels[j]], spacing_dim=3)
                 cost = self.e_perf * dist
 
                 self.ips_costs[i][j] = cost
 
         # optimal route
-        route, cost, _ = tsp.tsp_problem(ip_labels, self.ips_costs)
+        route, cost, _ = tsp.tsp_problem(ips_labels, self.ips_costs)
 
         rospy.loginfo('%s: found inspection route:\n%s', self.name, route)
 
         # generate action sequence
-        for r in route:
+        #   first hover on spot (with los for first IP)
+
+        next_pose = np.copy(self.pos)
+        next_pose[5] = tt.calculate_orientation(self.pos, self.ips[route[0]])
+
+        self.actions.append({
+            'name': 'hover',
+            'params': {
+                'pose': next_pose.tolist()
+            }
+        })
+
+        for n in xrange(len(route)):
             self.actions.append({
                 'name': 'goto',
                 'params': {
-                    'pose': self.ips[r]
+                    'pose': self.ips[route[n]].tolist()
+                }
+            })
+
+            next_pose = np.copy(self.ips[route[n]])
+
+            if n < len(route) - 1:
+                next_pose[5] = tt.calculate_orientation(self.ips[route[n]], self.ips[route[n + 1]])
+
+            self.actions.append({
+                'name': 'hover',
+                'params': {
+                    'pose': next_pose.tolist()
                 }
             })
 
         rospy.loginfo('%s: generated action sequence (n=%d)', self.name, len(self.actions))
 
-        print(self.actions)
+        # save mission plan
+        plan_file = os.path.join(self.output_dir, '{}_plan_{}.json'.format(self.output_label, self.plan_id))
+
+        plan = {
+            'ips_count': k,
+            'ips_label': ips_labels,
+            'ips_state': self.ips_costs.tolist(),
+            'time': time.time(),
+            'actions': self.actions,
+        }
+
+        with open(plan_file, 'wt') as plog:
+            plog.write(json.dumps(plan))
+
+        # increase plan counter (to keep track of future replans)
+        self.plan_id += 1
+
 
     def run(self):
         # mission init
@@ -343,7 +416,6 @@ class MissionExecutor(object):
             self.r_main.sleep()
 
 
-
 def parse_arguments(args=None):
     parser = argparse.ArgumentParser(
         description='Utility for running inspection missions with the executor module.',
@@ -354,7 +426,7 @@ def parse_arguments(args=None):
     parser.add_argument('config', type=str, help='Mission config file (JSON format).')
 
     # output group
-    parser.add_argument('--output', default='~', help='Output dir to save mission logs.')
+    parser.add_argument('--output', default=None, help='Output dir to save mission logs.')
     parser.add_argument('--label', default='current', help='Optional comment to add to the result file.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print detailed information.')
 
@@ -376,8 +448,8 @@ def main():
     output_dir = args.output
     output_label = args.label
 
-    if output_dir == '~':
-        output_dir = os.path.expanduser('~')
+    if output_dir is None:
+        output_dir = os.getcwd()
 
     # start ros node
     rospy.init_node('mission_executor')
@@ -395,16 +467,14 @@ def main():
         rospy.logfatal('%s could not load the cpnfig file (%s)', name, config_file)
         sys.exit(-1)
 
-    current_config = dict()
-    current_config.update({
+    ex_config = {
         'output_dir': output_dir,
         'output_label': output_label
-    })
-    current_config.update(config)
+    }
 
     # init the executor
-    me = MissionExecutor()
-    me.execute_mission(current_config)
+    me = MissionExecutor(**ex_config)
+    me.execute_mission(config)
 
     # start the mission
     me.run()
